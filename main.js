e65c59ba8aba17376a5ffd338df146441fcdcb0d
MNkomazana <nkomazanameluleki@gmail.com>
2021-04-05 12:06:31 +0200

    Another commit

--- main.js
+++ main.js
@@ -17,6 +17,26 @@ function checkForm(){
         passMsge.innerHTML = "Success";
     }
 }
-
+// Login button uses the function checkform to validate fields
 const btn = document.getElementById("login");
 btn.addEventListener("click",checkForm);
+// code ends here for the button
+
+
+// this function will be used in the validation of the form to create an account
+function createAcc(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let strEmail = document.getElementById("email1").value;
+    let strPass = document.getElementById("password1").value;
+    let strPassConfirm = document.getElementById("passwordConfirm").value;
+
+    if(strPassConfirm != strPass){
+        console.log("Meluleki Nkomz u r the best");
+    }
+
+}
+//function ends here
+
+const btnSubmit = document.getElementById("submit").value;
+btnSubmit.addEventListener("click", createAcc);
